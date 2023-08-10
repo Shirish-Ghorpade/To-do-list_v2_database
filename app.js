abaabe6994774eda57331d056b0af16f4a692f80
Shirish-Ghorpade <97929917+Shirish-Ghorpade@users.noreply.github.com>
2023-08-10 23:09:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -155,6 +155,6 @@ app.post("/delete", function (req, res) {
 //   res.render("about");
 // });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started on port 3000");
 });
